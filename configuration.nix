e3102d2f2477c6d74a5c4e795cc458508c9e86ec
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2024-11-04 22:52:06 +0000

    weston working

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,7 @@
 
 {
   imports = [
-    (modulesPath + "/profiles/all-hardware.nix")
+    #(modulesPath + "/profiles/all-hardware.nix")
     ./disko.nix
   ];
 
@@ -14,20 +14,20 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "tfc"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "Atlantic/Reykjavik";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
   console =  {
     earlySetup = true;
     font = "ter-v16n";
@@ -36,27 +36,54 @@
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     tree
-  #   ];
-  # };
+  users.users.tfc = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    password = "tfc";
+    shell = "${pkgs.fish}/bin/fish";
+    packages = with pkgs; [
+      tree
+    ];
+  };
+  nixpkgs.config.allowUnfree = true;
+  hardware.enableAllFirmware = true;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      # OpenGL Drivers
+      mesa
+
+      # Vulkan Drivers
+      vulkan-loader
+
+      # VAAPI Drivers (Video Acceleration API)
+      intel-media-driver  # Required for VAAPI on newer Intel GPUs
+
+      # Additional Drivers for Specific GPU Versions
+      vpl-gpu-rt          # for newer GPUs on NixOS >24.05 or unstable
+    ];
+  };
+  # hardware.videoDrivers = [ "intel" ]; # this does not work, this option is non existent
+
+
+  users.users.root.password = "root";
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    libinput
+    libseat
+    weston
+  ];
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
   # Automatically log in at the virtual consoles.
-  services.getty.autologinUser = "root";
+  services.getty.autologinUser = "tfc";
 
   # Allow the user to log in as root without a password.
   users.users.root.initialHashedPassword = "";
