0d1abc71aaeac27a64359c5646941dbdd286aa73
Jón Bjarni Bjarnason <jon@centroid.is>
2024-12-16 12:20:32 +0000

    add efi sys mount location

--- base-configuration.nix
+++ base-configuration.nix
@@ -17,6 +17,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
   boot.loader.timeout = 0;
 
   networking.hostName = "tfc"; # Define your hostname.
